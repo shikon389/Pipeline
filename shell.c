d41865c4a6788b4b42fbf6b0b8a9513f64c14f21
MeganMurray <megansmurray@gmail.com>
2015-03-21 22:11:37 -0400

    code for backend works - barebones. Needs to work for all cases

--- shell.c
+++ shell.c
@@ -2,6 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 #include "shell.h"
+#include <unistd.h>
 
 tknzr *tokenizer;
 
@@ -21,8 +22,11 @@ int promptUser()
 	printf("%s\n", prompt_input);
 
 	if (strcmp(prompt_input, "quit")) {
+		printf("if %s\n", prompt_input);
 		return 1;
-	} else {
+	} 
+	else {
+		printf("else %s\n", prompt_input);
 		tokenize();
 		return 0;
 	}
@@ -139,8 +143,165 @@ void initTokenizer()
 	}
 }
 
+
+
+
+
+
+/* Megan attempts to execute the tokenized commands
+*
+* Three cases:  First item
+*				Middle Item(s)
+*				Last Item
+*
+* Whatever is written to fd[1] will be read from fd[0].
+* 			[(0) INPUT]  [(1) OUTPUT]
+*/
+//char *cmds[][] = { {"ls", 0}, {"find", "main.c", 0}, { "find", "shell.c", 0}};
+char *cmd1[] = { "ls", 0 };
+char *cmd2[] = { "grep", "s", 0 };
+char *cmd3[] = { "grep", ".c", 0 };
+
+//right now this runs first, middle, and last.
+//this needs to be set up in a way that:
+//	if args = 1, only run last
+//  if args = 2, only run first and last
+//  if args >= 3, run first, then run middle until there is only one arg left, then run last on it
+
+void runfirst(int pfd1[]);
+void runmiddle(int pfd1[], int pfd2[]);
+void runlast(int pfd[]);
+
+void
+runfirst(int pfd1[])	/* run the first part of the pipeline, cmd1 */
+{
+	int pid;	/* we don't use the process ID here, but you may wnat to print it for debugging */
+
+	switch (pid = fork()) {
+
+	case 0: /* child */
+		//dup2(pfd[0], 0);  /*Put the first command here just to print it to the screen */
+		//execvp(cmd1[0], cmd1);  /*Run it so we know what to print*/
+
+		dup2(pfd1[1], 1);	/* now we actually put it where we need it to be */
+		close(pfd1[0]); 		/* this process don't need the other end */
+		close(pfd1[1]);
+		execvp(cmd1[0], cmd1);	/* run the command */
+		perror(cmd1[0]);	/* if it failed */
+		break;
+
+	default: /* parent does nothing */
+		//printf("%d\n", pid );
+		break;
+
+	case -1:
+		perror("fork");
+		exit(1);
+	}
+}
+
+void
+runmiddle(int pfd1[], int pfd2[])	/* run the first part of the pipeline, cmd2 */
+{
+	int pid;	/* we don't use the process ID here, but you may wnat to print it for debugging */
+
+	switch (pid = fork()) {
+
+	case 0: /* child */
+		//dup2(pfd[0], 0);  /*Put the first command here just to print it to the screen */
+		//execvp(cmd2[0], cmd2);  /*Run it so we know what to print*/
+
+		dup2(pfd1[0], 0);	/* now we actually put it where we need it to be */
+		dup2(pfd2[1], 1);
+		close(pfd1[0]);
+		close(pfd1[1]);
+		close(pfd2[0]);
+		close(pfd2[1]);
+		execvp(cmd2[0], cmd2);	/* run the command */
+		perror(cmd2[0]);	/* if it failed */
+		break; 
+
+	default: /* parent does nothing */
+		//printf("%d\n", pid );
+		break;
+
+	case -1:
+		perror("fork");
+		exit(1);
+	}
+}
+
+void
+runlast(int pfd[])	/* last thing in the pipeline (cmd3) */
+{
+	int pid;
+
+	switch (pid = fork()) {
+
+	case 0: /* child */
+		dup2(pfd[0], 0);	/* this end of the pipe becomes the standard input */
+		close(pfd[0]);
+		close(pfd[1]);		/* this process doesn't need the other end */
+		execvp(cmd3[0], cmd3);	/* run the command */
+		perror(cmd3[0]);	/* if it failed! */
+		break;
+
+	default: /* parent does nothing */
+		//printf("%d\n", pid );
+		break;
+
+	case -1:
+		perror("fork");
+		exit(1);
+	}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 int main(int argc, char **argv)
 {
+	int pid, status;
+	int fd1[2];
+	int fd2[2];
+
+
+	pipe(fd1);
+	pipe(fd2);
+
+	runfirst(fd1);
+
+	runmiddle(fd1, fd2);
+
+	close(fd1[0]); 
+	close(fd1[1]); /*this is important! close both file descriptors on the pipe */
+
+	runlast(fd2);
+
+
+	close(fd2[0]);
+	close(fd2[1]);
+
+	while ((pid = wait(&status)) != -1)	/* dead kids */
+		fprintf(stderr, "process %d exits with %d\n", pid, WEXITSTATUS(status));
+	exit(0);
+
+
+
+
+
+
+
 	initTokenizer();
 
 	while (promptUser()) {
